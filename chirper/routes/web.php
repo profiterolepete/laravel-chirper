939b37e8d9687722ee70ed7f4b0922d63d73362f
Pete McGinty <pete@practicehub.io>
2024-02-27 13:05:01 +0000

    added routes to index new chirps and retrieve existing chirps

--- chirper/routes/web.php
+++ chirper/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store'])
+    ->middleware(['auth', 'verified']);
